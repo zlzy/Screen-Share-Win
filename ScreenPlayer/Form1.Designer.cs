bc4dba8c2f48bac6051b2683395baf9dd7687bee
zlzy <35788282+zlzy@users.noreply.github.com>
2019-12-08 15:17:57 -0800

    ScreenPlayer

---
+++ ScreenPlayer/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace ScreenPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbx_render = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_render)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbx_render
+            // 
+            this.pbx_render.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbx_render.Location = new System.Drawing.Point(0, 0);
+            this.pbx_render.Name = "pbx_render";
+            this.pbx_render.Size = new System.Drawing.Size(441, 363);
+            this.pbx_render.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx_render.TabIndex = 0;
+            this.pbx_render.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 363);
+            this.Controls.Add(this.pbx_render);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Screen Player";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_render)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbx_render;
+    }
+}
+
---
+++ ScreenPlayer/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace ScreenPlayer
+{
+    public partial class Form1 : Form
+    {
+        byte[] m_buf;
+        int m_len;
+        byte[] m_tmpBuf;
+        byte[] endFlagbuf;
+        byte[] endFlag;
+        public Form1()
+        {
+            InitializeComponent();
+            m_buf = new byte[1024 * 1024 * 10];
+            m_tmpBuf = new byte[1024 * 1024 * 10];
+            endFlagbuf = new byte[4];
+            endFlag = new byte[4];
+            endFlag[0] = 0x0d;
+            endFlag[1] = 0x0a;
+            endFlag[2] = 0x0d;
+            endFlag[3] = 0x0a;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this.TopMost = true;
+
+            TcpListener l = new TcpListener(IPAddress.Any, 7788);
+            l.Start();
+
+            Thread t = new Thread(new ThreadStart(() => {
+                while (true)
+                {
+                    Socket s = l.AcceptSocket();
+                    Stream network = new NetworkStream(s);
+                    network.BeginRead(m_tmpBuf, 0, m_tmpBuf.Length, ReceiveCallback, network);
+                }
+            }));
+            t.IsBackground = true;
+            t.Start();
+        }
+
+        void InvokeHelper(MethodInvoker invoker)
+        {
+            try
+            {
+                if (InvokeRequired)
+                    Invoke(invoker);
+                else
+                    invoker.Invoke();
+            }
+            catch { }
+        }
+
+        void ReceiveCallback(IAsyncResult ar)
+        {
+            NetworkStream network = (NetworkStream)ar.AsyncState;
+            int len = network.EndRead(ar);
+            bool isend = false;
+            int lackLen = 0;
+            for (int i = 0; i < len - 3; i++)
+            {
+                Buffer.BlockCopy(m_tmpBuf, i, endFlagbuf, 0, 4);
+                if (endFlag.SequenceEqual(endFlagbuf))
+                {
+                    isend = true;
+                    lackLen = i;
+                }
+            }
+            if (!isend)
+            {
+                Buffer.BlockCopy(m_tmpBuf, 0, m_buf, m_len, len);
+                m_len += len;
+            }
+            else
+            {
+                Buffer.BlockCopy(m_tmpBuf, 0, m_buf, m_len, lackLen);
+                m_len += lackLen;
+                try
+                {
+                    MemoryStream ms = new MemoryStream(m_buf, 0, m_len);
+                    Image img = Bitmap.FromStream(ms);
+                    InvokeHelper(() =>
+                    {
+                        if (pbx_render.Image != null) pbx_render.Image.Dispose();
+                        pbx_render.Image = img;
+                    });
+                }
+                catch(Exception ex) { Console.WriteLine(ex.Message); }
+
+                m_len = 0;
+            }
+            if (m_len > 1024 * 1024 * 5)
+                m_len = 0;
+
+            try { network.BeginRead(m_tmpBuf, 0, m_tmpBuf.Length, ReceiveCallback, network); }catch{}
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                Close();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+        }
+    }
+}
+
+
