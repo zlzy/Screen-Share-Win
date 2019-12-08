7807c95d37e7b09e696b81393ab1c5b3f5f9dda8
zlzy <35788282+zlzy@users.noreply.github.com>
2019-12-08 15:27:50 -0800

    Streamer

---
+++ ScreenStreamer/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace ScreenStreamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_ip1 = new System.Windows.Forms.TextBox();
+            this.txt_ip2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_ip4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_ip3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Top Left :";
+            // 
+            // txt_ip1
+            // 
+            this.txt_ip1.Location = new System.Drawing.Point(82, 16);
+            this.txt_ip1.Name = "txt_ip1";
+            this.txt_ip1.Size = new System.Drawing.Size(100, 20);
+            this.txt_ip1.TabIndex = 2;
+            // 
+            // txt_ip2
+            // 
+            this.txt_ip2.Location = new System.Drawing.Point(263, 17);
+            this.txt_ip2.Name = "txt_ip2";
+            this.txt_ip2.Size = new System.Drawing.Size(100, 20);
+            this.txt_ip2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Top Right :";
+            // 
+            // txt_ip4
+            // 
+            this.txt_ip4.Location = new System.Drawing.Point(263, 43);
+            this.txt_ip4.Name = "txt_ip4";
+            this.txt_ip4.Size = new System.Drawing.Size(100, 20);
+            this.txt_ip4.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(189, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Bottom Right:";
+            // 
+            // txt_ip3
+            // 
+            this.txt_ip3.Location = new System.Drawing.Point(82, 42);
+            this.txt_ip3.Name = "txt_ip3";
+            this.txt_ip3.Size = new System.Drawing.Size(100, 20);
+            this.txt_ip3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Bottom Left :";
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(288, 76);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 9;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 108);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.txt_ip4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_ip3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_ip2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_ip1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Screen Streamer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_ip1;
+        private System.Windows.Forms.TextBox txt_ip2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_ip4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_ip3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_start;
+    }
+}
+
---
+++ ScreenStreamer/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.Net;
+using GodSharp.Sockets;
+
+namespace ScreenStreamer
+{
+    public partial class Form1 : Form
+    {
+        TcpClient[] m_socs;
+        Thread[] m_threads;
+
+        string[] m_ipAddresses;
+        bool m_isRunning;
+        public Form1()
+        {
+            InitializeComponent();
+            m_socs = new TcpClient[4];
+            m_threads = new Thread[4];
+            m_ipAddresses = new string[4];
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            if (m_isRunning)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (m_threads[i] != null)
+                        m_threads[i].Abort();
+                }
+                btn_start.Text = "Start";
+                m_isRunning = false;
+                EnableIPBox(true);
+            }
+            else
+            {
+                if (!ValidateIPAddress())
+                    return;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    if (m_ipAddresses[i].Length > 0)
+                        StartSend(i);
+                }
+                m_isRunning = true;
+                btn_start.Text = "Stop";
+                EnableIPBox(false);
+            }
+        }
+
+        void EnableIPBox(bool enable)
+        {
+            txt_ip1.Enabled = enable;
+            txt_ip2.Enabled = enable;
+            txt_ip3.Enabled = enable;
+            txt_ip4.Enabled = enable;
+        }
+        void StartSend(int idx)
+        {
+            m_threads[idx] = new Thread(new ThreadStart(() =>
+            {
+                while (true)
+                {
+                    if (m_socs[idx] != null)
+                    {
+                        if (m_socs[idx].Running)
+                        {
+                            Bitmap bmp = CaptureImage(idx);
+                            using (MemoryStream ms = new MemoryStream())
+                            {
+                                bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                                //ms.SetLength(2);
+                                byte[] buf = new byte[ms.Length + 4];
+                                buf[ms.Length] = 0x0d;
+                                buf[ms.Length + 1] = 0x0a;
+                                buf[ms.Length + 2] = 0x0d;
+                                buf[ms.Length + 3] = 0x0a;
+                                Console.WriteLine("Sent: {0}", buf.Length);
+                                Buffer.BlockCopy(ms.GetBuffer(), 0, buf, 0, (int)ms.Length);
+                                try
+                                {
+                                    int len = m_socs[idx].Connection.Send(buf);
+                                    Console.WriteLine("sent len: {0}", len);
+                                }
+                                catch {
+                                    m_socs[idx] = null;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        m_socs[idx] = new TcpClient(m_ipAddresses[idx], 7788);
+                        try
+                        {
+                            m_socs[idx].Start();
+                            m_socs[idx].OnDisconnected = (c) => { m_socs[idx] = null; };
+                            m_socs[idx].OnException = (c) => { m_socs[idx] = null; };
+                            m_socs[idx].OnStopped = (c) => { m_socs[idx] = null; };
+                        }
+                        catch {
+                            m_socs[idx] = null;
+                            Thread.Sleep(3000);
+                        }
+                        
+                    }
+                    Thread.Sleep(40);
+                }
+            }));
+            m_threads[idx].IsBackground = true;
+            m_threads[idx].Start();
+        }
+
+        bool ValidateIPAddress() {
+            for (int i = 0; i < 4; i++)
+                m_ipAddresses[i] = "";
+            if (txt_ip1.Text.Length > 0)
+            {
+                try
+                {
+                    IPAddress.Parse(txt_ip1.Text);
+                    m_ipAddresses[0] = txt_ip1.Text;
+                }
+                catch
+                {
+                    MessageBox.Show("Please input correct ip address in top left");
+                    return false;
+                }
+            }
+            if (txt_ip2.Text.Length > 0)
+            {
+                try
+                {
+                    IPAddress.Parse(txt_ip2.Text);
+                    m_ipAddresses[1] = txt_ip2.Text;
+                }
+                catch
+                {
+                    MessageBox.Show("Please input correct ip address in top right");
+                    return false;
+                }
+            }
+            if (txt_ip3.Text.Length > 0)
+            {
+                try
+                {
+                    IPAddress.Parse(txt_ip3.Text);
+                    m_ipAddresses[2] = txt_ip3.Text;
+                }
+                catch
+                {
+                    MessageBox.Show("Please input correct ip address in bottom left");
+                    return false;
+                }
+            }
+            if (txt_ip4.Text.Length > 0)
+            {
+                try
+                {
+                    IPAddress.Parse(txt_ip4.Text);
+                    m_ipAddresses[3] = txt_ip4.Text;
+                }
+                catch
+                {
+                    MessageBox.Show("Please input correct ip address in bottom right");
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static Bitmap CaptureImage(int idx)
+        {
+            Rectangle bounds = Screen.GetBounds(Screen.GetBounds(Point.Empty));
+            Bitmap bitmap = new Bitmap(bounds.Width / 2, bounds.Height / 2);
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                switch (idx)
+                {
+                    case 0:
+                        g.CopyFromScreen(new Point(0, 0), Point.Empty, bounds.Size);
+                        break;
+                    case 1:
+                        g.CopyFromScreen(new Point(bounds.Width / 2, 0), Point.Empty, bounds.Size);
+                        break;
+                    case 2:
+                        g.CopyFromScreen(new Point(0, bounds.Height / 2), Point.Empty, bounds.Size);
+                        break;
+                    case 3:
+                        g.CopyFromScreen(new Point(bounds.Width / 2, bounds.Height / 2), Point.Empty, bounds.Size);
+                        break;
+                }
+                
+            }
+            return bitmap;
+        }
+    }
+}
